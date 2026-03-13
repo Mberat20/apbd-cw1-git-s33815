912d87825d17ef8b6ca8fa8e676ae54cabf800db
mustafa berat bala <s33815@pjwstk.edu.pl>
2026-03-13 21:26:49 +0100

    Add separate error message for invalid input

--- Program.cs
+++ Program.cs
@@ -1,3 +1,9 @@
 ﻿Console.WriteLine("Welcome to the Statistics Calculator");
 Console.WriteLine("Please enter a number to process:");
 string input = Console.ReadLine();
+
+if (string.IsNullOrWhiteSpace(input))
+{
+    Console.WriteLine("Error:Input can not be empty.Please try again");
+    return;
+}
