9ae1380f140673c03b8147e082e52c3bd66b1b27
mustafa berat bala <s33815@pjwstk.edu.pl>
2026-03-13 21:31:31 +0100

    Add CalculateAverage method

--- StatisticsHelper.cs
+++ StatisticsHelper.cs
@@ -1,6 +1,21 @@
-﻿namespace apbd_cw1_git_s33815;
+﻿using System;
 
-public class StatisticsHelper
+namespace apbd_cw1_git_s33815
 {
-    
+    public class StatisticsHelper
+    {
+        public static double CalculateAverage(int[] values)
+        {
+            if (values == null || values.Length == 0)
+                return 0;
+
+            int sum = 0;
+            foreach (int number in values)
+            {
+                sum += number;
+            }
+
+            return (double)sum / values.Length;
+        }
+    }
 }
