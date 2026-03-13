e7597945dca6418495dba0ec79c63f5398bf6cd4
mustafa berat bala <s33815@pjwstk.edu.pl>
2026-03-13 21:37:56 +0100

    Add CalculateMax method

--- StatisticsHelper.cs
+++ StatisticsHelper.cs
@@ -17,5 +17,21 @@ namespace apbd_cw1_git_s33815
 
             return (double)sum / values.Length;
         }
+
+        public static int CalculateMax(int[] numbers)
+        {
+            if (numbers == null || numbers.Length == 0)
+                throw new ArgumentException("Array cannot be empty");
+
+            int max = numbers[0];
+            foreach (int number in numbers)
+            {
+                if (number > max)
+                {
+                    max = number;
+                }
+            }
+            return max;
+        }
     }
 }
